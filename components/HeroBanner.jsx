03f75f3d543ff2d76c24d18c06613c367d96a58f
Daniel <danielcapcelea@mail.ru>
2022-05-02 19:48:16 +0200

    connect sanity

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import Link from 'next/link';
 
-const HeroBanner = () => {
+import {urlFor} from "../lib/client";
+
+const HeroBanner = ({heroBanner}) => {
     return (
-        <div>
-            hero banner
+        <div className='hero-banner-container'>
+            <div>
+                <p className='beats-solo'>
+                    {heroBanner.smallText}
+                </p>
+                <h3>{heroBanner.midText}</h3>
+                <h1>{heroBanner.largeText1}</h1>
+                <img
+                    className='hero-banner-image'
+                    src={urlFor(heroBanner.image)}
+                    alt='headphones'/>
+                <div>
+                    <Link href={`/product/${heroBanner.product}`}>
+                        <button type='button'>
+                            {heroBanner.buttonText}
+                        </button>
+                    </Link>
+                    <div className='desc'>
+                        <h5>
+                            {heroBanner.desc}
+                        </h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default HeroBanner;
-    
