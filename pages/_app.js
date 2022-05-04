1584a4166d8769741442d30875a98de33a160204
Daniel <danielcapcelea@mail.ru>
2022-05-04 22:29:00 +0200

    adding layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {Layout} from "../components";
+
+function MyApp({Component, pageProps}) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    )
+};
 
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
 export default MyApp;
