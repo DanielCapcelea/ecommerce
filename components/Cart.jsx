708c013b7ec7e37264b352aad1faa617bee2bf4f
Daniel <danielcapcelea@mail.ru>
2022-05-07 19:31:33 +0200

    integrating stripe/ getStripe

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -5,10 +5,31 @@ import {TiDeleteOutline} from "react-icons/ti";
 import {toast} from "react-hot-toast";
 import {useStateContext} from "../stateContext/StateContext";
 import {urlFor} from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
     const cartRef = useRef();
-    const {totalPrice, onRemove, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity} = useStateContext();
+    const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
+
+    const handleCheckout = async () => {
+        const stripe = await getStripe();
+
+        const response = await fetch('/api/stripe', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cartItems),
+        });
+
+        if(response.statusCode === 500) return;
+
+        const data = await response.json();
+
+        toast.loading('Redirecting...');
+
+        stripe.redirectToCheckout({ sessionId: data.id });
+    }
 
     return (
         <div className='cart-wrapper' ref={cartRef}>
@@ -94,7 +115,7 @@ const Cart = () => {
                             <button
                                 type='button'
                                 className='btn'
-                                onClick=''
+                                onClick={handleCheckout}
                             >
                                 Pay with Stripe
                             </button>
--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,9 +2,41 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
+        console.log(req.body.cartItems)
+
+
         try {
-            // Create Checkout Sessions from body params.
-            const session = await stripe.checkout.sessions.create({
+            const params = {
+                submit_type: 'pay',
+                mode: 'payment',
+                payment_method_types: ['card'],
+                billing_address_collection: 'auto',
+                shipping_options: [
+                    {shipping_rate: 'shr_1KwqcsIYpuODogXknktXKz95'},
+                    {shipping_rate: 'shr_1KwqdgIYpuODogXkPPs8Fl4b'},
+                    {shipping_rate: 'shr_1KwqfVIYpuODogXklAa0EMfy'},
+                ],
+                line_item: req.body.cartItems.map((item) => {
+                    const img = item.image[0].asset._ref;
+                    const newImage = img.replace('image-',
+                        'https://cdn.sanity.io/images/x7zg4rft/production/').replace(
+                        '-webp', '.webp');
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage],
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        quantity: item.quantity
+                    }
+                }),
                 line_items: [
                     {
                         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -12,10 +44,11 @@ export default async function handler(req, res) {
                         quantity: 1,
                     },
                 ],
-                mode: 'payment',
                 success_url: `${req.headers.origin}/?success=true`,
                 cancel_url: `${req.headers.origin}/?canceled=true`,
-            });
+            }
+            // Create Checkout Sessions from body params.
+            const session = await stripe.checkout.sessions.create(params);
             res.redirect(303, session.url);
         } catch (err) {
             res.status(err.statusCode || 500).json(err.message);
