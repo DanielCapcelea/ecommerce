9deceabbf387ce748011a91f6f4212dab5585460
Daniel <danielcapcelea@mail.ru>
2022-05-08 15:07:04 +0200

    fixing quantity update in the cart/ change quantity bug

--- stateContext/StateContext.js
+++ stateContext/StateContext.js
@@ -48,18 +48,30 @@ export const StateContext = ({children}) => {
 
     }
 
-    const toggleCartItemQuantity = (id, value) => {
-        foundProduct = cartItems.find((item) => item._id === id);
+    const toggleCartItemQuantity =(id, value) => {
+        foundProduct = cartItems.find(item => item._id === id);
         index = cartItems.findIndex((product) => product._id === id);
-        const newCartItems = cartItems.filter((item) => item._id !== id)
 
-        if (value === 'inc') {
-            setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity + 1}]);
-            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
-            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1)
+        if(value === 'inc') {
+            foundProduct.quantity += 1;
+            const newCartItems = cartItems.map((item) => {
+                if(foundProduct._id === item._id) return foundProduct;
+
+                return item;
+            });
+            setCartItems(newCartItems);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
         } else if (value === 'dec') {
-            if (foundProduct.quantity > 1) {
-                setCartItems([...newCartItems, {...foundProduct, quantity: foundProduct.quantity - 1}]);
+            if(foundProduct.quantity > 1){
+                foundProduct.quantity -= 1;
+                const newCartItems = cartItems.map((item) => {
+                    if(foundProduct._id === item._id) return foundProduct;
+
+                    return item;
+                })
+
+                setCartItems(newCartItems);
                 setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
                 setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
             }
